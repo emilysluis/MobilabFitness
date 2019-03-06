015af02aa7a55ee7fdbb865bbf046b5d5e97cd79
Emily <emily.sluis@gmail.com>
2019-03-06 17:05:36 +0100

    End of the day March 6

--- app/src/main/java/com/example/MobilabFitness/User/UserViewModel.java
+++ app/src/main/java/com/example/MobilabFitness/User/UserViewModel.java
@@ -1,23 +1,55 @@
 package com.example.MobilabFitness.User;
 
 import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 
 import java.util.List;
 
-public class UserViewModel {
-    private UserRepository mRepository;
+/**
+ * View Model to keep a reference to the user repository and
+ * an up-to-date list of all users.
+ */
 
+public class UserViewModel extends AndroidViewModel {
+
+    private UserRepository mRepository;
+    // Using LiveData and caching what getAlphabetizedUsers returns has several benefits:
+    // - We can put an observer on the data (instead of polling for changes) and only update the
+    //   the UI when the data actually changes.
+    // - Repository is completely separated from the UI through the ViewModel.
     private LiveData<List<User>> mAllUsers;
 
-    public UserViewModel (Application application) {
-        //TODO: uncomment line below
-        //  super(application);
+    public UserViewModel(Application application) {
+        super(application);
         mRepository = new UserRepository(application);
         mAllUsers = mRepository.getAllUsers();
     }
 
-    LiveData<List<User>> getAllUsers() { return mAllUsers; }
+    LiveData<List<User>> getAllUsers() {
+        return mAllUsers;
+    }
 
-    public void insert(User user) { mRepository.insert(user); }
+    void insert(User user) {
+        mRepository.insert(user);
+    }
 }
+
+/*
+public class UserViewModel {
+private UserRepository mRepository;
+
+private LiveData<List<User>> mAllUsers;
+
+public UserViewModel (Application application) {
+//TODO: uncomment line below
+//  super(application);
+mRepository = new UserRepository(application);
+mAllUsers = mRepository.getAllUsers();
+}
+
+LiveData<List<User>> getAllUsers() { return mAllUsers; }
+
+public void insert(User user) { mRepository.insert(user); }
+}
+*/
